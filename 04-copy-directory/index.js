251d7c770e4c51d50db24377fa0c1ec1148df018
alexey.krylov <turn2river@gmail.com>
2023-05-09 00:00:39 +0300

    feat: add solution for 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const pathToFolder = path.join(__dirname, 'files');
+const pathToFolderCopy = path.join(__dirname, 'files-copy');
 
+fs.mkdir(pathToFolderCopy, {recursive: true}, (error) => {
+  if (error) return console.error(error.message);
+});
+
+fs.readdir(pathToFolder, {withFileTypes: true}, function (error, files) {
+  if (error) return console.error(error.message);
+
+  files.forEach((file) => {
+    if (file.isFile()) {
+      fs.copyFile(path.join(pathToFolder, file.name), path.join(pathToFolderCopy, file.name), (error) => {
+        if (error) return console.error(error.message);
+      });
+    }
+  });
+});
